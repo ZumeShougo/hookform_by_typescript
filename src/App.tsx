2a2bc964625ee65cd9b3c985cfec8e05c7b9c3ce
ZumeShougo <g0109725rena@gmail.com>
2023-02-22 09:59:47 +0900

    Add error message

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ type FormInput ={
 
 export default function App() {
 
-  const { register, handleSubmit } = useForm<FormInput>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInput>();
   const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
 
   return (
@@ -18,10 +18,17 @@ export default function App() {
         <div>
           <label>名前</label>
           <input {...register('name', { required: true})} placeholder='名前'/>
+          {errors.name && <p className='error'>名前の入力は必須です</p>}
         </div>
         <div>
           <label>年齢</label>
-          <input {...register('age', { required: true})} placeholder='年齢'/>歳
+          <input {...register('age', {
+            required: { value: true, message: "年齢の入力は必須です"},
+            min: { value: 0, message: "最小数は0です"},
+            max: { value: 150, message: "最大値は150です"}
+            })} placeholder='年齢'
+          />歳
+          {errors.age && <p className='error'>{errors.age.message}</p>}
         </div>
         <div>
           <button type='submit'>送信</button>
