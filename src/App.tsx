b5af22ea6b5e6786c969a727f819dede49151427
ZumeShougo <g0109725rena@gmail.com>
2023-02-22 09:17:45 +0900

    Add form with useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './App.css';
 
-function App() {
+type FormInput ={
+  name: string;
+  age: number;
+};
+
+export default function App() {
+
+  const { register, handleSubmit } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>名前</label>
+          <input {...register('name', { required: true})} placeholder='名前'/>
+        </div>
+        <div>
+          <label>年齢</label>
+          <input {...register('age', { required: true})} placeholder='年齢'/>歳
+        </div>
+        <div>
+          <button type='submit'>送信</button>
+        </div>
+      </form>
     </div>
   );
 }
 
-export default App;
+
